093616d7c8296f3a5726905dbd7fbef53c21eef1
Gaurav10806 <gaurav10806@gmail.com>
2026-07-01 01:38:48 +0530

    Remove MongoDB connection test file

--- server/test.js
+++
@@ -1,6 +0,0 @@
-const dns = require("dns");
-
-dns.resolveSrv("_mongodb._tcp.cluster0.4viowuz.mongodb.net", (err, addresses) => {
-  console.log("Error:", err);
-  console.log("Addresses:", addresses);
-});
