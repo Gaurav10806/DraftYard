399fe9acbe5d38f5e2f3f00043a580f0b0359cd4
Gaurav10806 <gaurav10806@gmail.com>
2026-07-01 01:29:19 +0530

    Setup Express backend and connect MongoDB Atlas

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dns = require("node:dns");
+
+// Force Node to use public DNS servers
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+dns.setDefaultResultOrder("ipv4first");
+
+const connectDB = async () => {
+  try {
+    console.log("DNS Servers:", dns.getServers());
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:");
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
